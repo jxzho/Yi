14218646c2099d85964f246e2dd2f70820d0235e
junxio <879917302@qq.com>
2024-01-10 10:43:36 +0800

    feat: optimize some

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import SvgTranslation from '@/assets/translation.svg'
 import SvgLoading from '@/assets/loading.svg'
 import { YiParamsPayload } from '@/types'
 import UseTextArea from '@/components/textarea.vue'
+import Toast from '@/components/toast.vue'
 
 const lang = reactive({
   from: 'en',
@@ -52,7 +53,9 @@ const startTranslate = () => {
   
   fetch('https://translate.junxio220.workers.dev', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify(bodyPost.value)
   })
     .then(response => {
@@ -61,7 +64,6 @@ const startTranslate = () => {
       } else {
         throw new Error('Network response was not ok ' + response.statusText);
       }
-      
     })
     .then(data => {
       if (data) {
@@ -73,7 +75,7 @@ const startTranslate = () => {
       }
     })
     .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
+      console.error('There has been a problem with your fetch operation:', error)
     })
     .finally(() => {
       loading.value = false
@@ -91,6 +93,44 @@ const turnLang = () => {
   startTranslate()
 }
 
+const toastData = reactive({
+  type: 'success',
+  visible: false,
+  messages: ''
+})
+function copyTextToClipboard (text: string) {
+  var textArea = document.createElement('textarea')
+  textArea.value = text
+  textArea.style.position = 'fixed'
+  textArea.style.top = '0'
+  textArea.style.left = '0'
+  textArea.style.opacity = '0'
+
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+
+  try {
+    return document.execCommand('copy')
+  }
+  catch (err) {}
+  finally { document.body.removeChild(textArea) }
+  
+  return false
+}
+const startCopy = () => {
+  if (toValue.value) {
+    const flag = copyTextToClipboard(toValue.value)
+    toastData.messages = flag ? '已复制结果' : '请尝试手动复制'
+    toastData.type = flag ? 'success' : 'warning'
+    toastData.visible = true
+    setTimeout(() => {
+      toastData.messages = ''
+      toastData.visible = false
+    }, 2000)
+  }
+}
+
 </script>
 
 <template>
@@ -102,8 +142,11 @@ const turnLang = () => {
     <div class="to">
       <TransitionGroup name="fade">
         <img v-if="loading" class="max-w-[88px]" :src="SvgLoading" />
-        <div v-if="!loading" class="underline decoration-dotted">
+        <div v-if="!loading && toValue" class="underline decoration-dotted">
           {{ toValue }}
+          <i class="icon-copy" @click="startCopy">
+            <svg width="18" height="18" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 12.432v-4.62A2.813 2.813 0 0 1 15.813 5h24.374A2.813 2.813 0 0 1 43 7.813v24.375A2.813 2.813 0 0 1 40.187 35h-4.67" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M32.188 13H7.811A2.813 2.813 0 0 0 5 15.813v24.374A2.813 2.813 0 0 0 7.813 43h24.375A2.813 2.813 0 0 0 35 40.187V15.814A2.813 2.813 0 0 0 32.187 13z" stroke="#333" stroke-width="2" stroke-linejoin="round"/></svg>
+          </i>
         </div>
       </TransitionGroup>
     </div>
@@ -126,6 +169,15 @@ const turnLang = () => {
       </UseTextArea>
     </div>
   </div>
+  
+  <Teleport to="body">
+    <Toast
+      :type="toastData.type as 'warning' | 'success'"
+      :visible="toastData.visible"
+      :message="toastData.messages"
+      @close="toastData.visible = false;"
+    />
+  </Teleport>
 </template>
 
 <style lang="scss" scoped>
@@ -146,5 +198,9 @@ const turnLang = () => {
 
 .btn-turn {
   @apply cursor-pointer flex justify-center items-center;
+}
+
+.icon-copy {
+  @apply inline-block ml-1.5 cursor-pointer;
 }
 </style>
